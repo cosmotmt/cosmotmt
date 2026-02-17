a6da4ea9c9dd3ff5e9a2689e09cb729adfe15fcc
cosmotmt <saitokazumasa.jp@gmail.com>
2026-02-17 20:54:59 +0900

    [update] platform初期データ追加

--- app/migrations/0004_create_platforms.sql
+++ app/migrations/0004_create_platforms.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS platforms (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL UNIQUE
 );
+
+-- 初期データの投入
+INSERT OR IGNORE INTO platforms (name) VALUES ('PC');
+INSERT OR IGNORE INTO platforms (name) VALUES ('Windows');
+INSERT OR IGNORE INTO platforms (name) VALUES ('Mac');
+INSERT OR IGNORE INTO platforms (name) VALUES ('iOS');
+INSERT OR IGNORE INTO platforms (name) VALUES ('Android');
+INSERT OR IGNORE INTO platforms (name) VALUES ('Web');
