6b3ab96f427f31f7ba41b4bb623a0576a072e0a1
cosmotmt <saitokazumasa.jp@gmail.com>
2026-02-20 00:58:15 +0900

    [fix] OGP が反映されない不具合を修正

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,6 @@
 export const runtime = "edge";
 
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://cosmotmt.jp"),
   title: {
     default: "CosmoTmt Planet",
     template: "%s | CosmoTmt Planet"
